285d0efcf9a2ba96103c9ff66a4eb8e4e039e475
Jake Bixby <trabus@gmail.com>
2015-03-22 16:58:37 -0700

    extend example

---
+++ app/components/far-bar/component.js
@@ -0,0 +1,6 @@
+import FooBar from 'extend-component/components/foo-bar/component';
+// import layout from 'extend-component/components/foo-bar/template';
+
+export default FooBar.extend({
+  // layout: layout
+});
